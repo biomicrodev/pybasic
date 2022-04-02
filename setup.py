7774336bdd2c173c2a7114a10617cbeed3d7a284
Sebastian Ahn <wooahns@gmail.com>
2022-04-02 16:40:15 -0400

    Added setup.py file.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="pybasic",
+    version="0.0.1",
+    description="Background and shading correction for microscopy images",
+    url="https://github.com/biomicrodev/pybasic",
+    author="Sebastian Ahn",
+    license="MIT",
+)
